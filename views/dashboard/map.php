8b8077d4e77712d7ff9624675333f27e4f67ceb4
JaimeBotet <jaime.90.botet@gmail.com>
2021-02-18 13:19:33 +0100

    clean the iframe

--- views/dashboard/map.php
+++ views/dashboard/map.php
@@ -21,31 +21,11 @@ require "views/components/head.php";
             }
         }
         ?>
-        <div class="map_display">
-
-            <div class="mapouter">
-                <div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://yt2.org/youtube-to-mp3-ALeKk00qEW0sxByTDSpzaRvl8WxdMAeMytQ1611842368056QMMlSYKLwAsWUsAfLipqwCA2ahUKEwiikKDe5L7uAhVFCuwKHUuFBoYQ8tMDegUAQCSAQCYAQCqAQdnd3Mtd2l6"></a><br>
-                    <style>
-                        .mapouter {
-                            position: relative;
-                            text-align: right;
-                            height: 500px;
-                            width: 600px;
-                        }
-                    </style>
-                    <style>
-                        .gmap_canvas {
-                            overflow: hidden;
-                            background: none !important;
-                            height: 500px;
-                            width: 600px;
-                        }
-                    </style>
-                </div>
+        <div class="mapouter">
+            <div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><br>
             </div>
-            <!-- <iframe src="/maps/documentation/javascript/examples/full/mysql-to-maps" height="480px" width="100%"></iframe> -->
         </div>
-
+        <br>
         <button type="button" class="btn btn-light"><a href="?controller=user&action=showDashboard">Back</a></button>
     </main>
 </body>
