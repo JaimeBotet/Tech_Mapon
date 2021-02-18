7a1fccdfee1f7ee8b6b46912149e158d5b259caf
JaimeBotet <jaime.90.botet@gmail.com>
2021-02-18 01:18:25 +0100

    Create Date and Time Form

--- controllers/mapController.php
+++ controllers/mapController.php
@@ -4,8 +4,16 @@ require_once MODELS . "mapModel.php";
 
 $action;
 
+$action;
 if (isset($_REQUEST["action"])) {
     $action = $_REQUEST["action"];
-} else {
-    $action = "getAllEmployees";
+}
+if (function_exists($action)) {
+    call_user_func($action, $_REQUEST);
+}
+
+function getCarRoute()
+{
+    $dateFrom = $_REQUEST['dateFrom'];
+    $dateTo = $_REQUEST['dateTo'];
 }
