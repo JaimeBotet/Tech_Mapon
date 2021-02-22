d15edfce046b990e7a8f12e3926ded140403001b
JaimeBotet <jaime.90.botet@gmail.com>
2021-02-23 00:00:36 +0100

    Done

--- README.md
+++ README.md
@@ -16,7 +16,13 @@ Then hit ENTER, and then execute the SQL file:
     source ./config/db.sql
 ```
 
-And the database will be created and populated.
+And the database will be created and populated with 3 users and passwords:
+
+| User  | Password |
+| ----- | -------- |
+| Jaime | test1234 |
+| Jose  | test4321 |
+| Juan  | test1324 |
 
 ## The architecture 🚀
 
@@ -25,6 +31,12 @@ Im using a MVC architecture where I use the _index.php_ file as an entry point w
 The authentication process is very simple, I created some dummy data to populate the **MySQL** DataBase including name and password, to get logged in. If the data is correct, then the next screen will be shown, where we are going to be able to select the time range we want to see the route.
 
 Since the authentication process is very simple, Im not using the **SESSION** feature and thus, there is not a _logout_ functionality; but it would advisable to do so if we want to make a more robust application.
+
+## The Google Maps API 🚀
+
+To be able to access the data gathered from the _mapon_ API first we make a PHP **HTTP** request with _curl_ and save the data from the routes in the file _/resources/polyline.json_ and then we retrieve it back in the _/assets/js/googleMaps.js_ with the _$.getJSON_ function. I could have stored it in the DB, but this way was a quick fix.
+
+Once we have obtained the routes in the **JavaScript** we can then loop through the routes and distinguish if it is a polyline or a stopping point, this way we can decide weather to call the _addMarker()_ function (to add the point were the vehicle stopped) or the _addPolyline()_ function (to draw the route that the vehicle did).
 
 ## Author ✒️
 
--- assets/js/googleMaps.js
+++ assets/js/googleMaps.js
@@ -1,139 +1,44 @@
-// function initMap() {
-//     console.log("initializing map!");
-
-//     var options = {
-//         zoom: 13,
-//         center: {lat:41.38879, lng: 2.15899}
-//     }
-//     console.log(options);
-//     var map = new google.maps.Map(document.getElementById('map_canvas'), options);
-//     console.log(map);
-
-//     $.getJSON("./resources/polyline.json", function(routes) {
-//         console.log(routes);
-
-//         var completeRoute = "";
-//         routes.forEach((route) => {
-//             if (route.type === "route") {
-//                 completeRoute += route.polyline;
-//             }
-//             else if(route.type === "stop"){
-//                 var pos = {
-//                     lat: route.start.lat,
-//                     lng: route.start.lng
-//                 }
-//                 var marker = new google.maps.Marker({
-//                     map: map,
-//                     draggable: false,
-//                     position: pos,
-//                     icon: 'https://www.freeiconspng.com/uploads/stop-sign-png-14.png'
-
-//                 })
-                
-//                 var infoWindow = new google.maps.infoWindow({content: 'Vehicle Stop' });
-
-//                 marker.addListener('click', ()=>{
-//                     infoWindow.open(map, marker);
-//                 })
-//             }
-//         })
-//         var polyline = new google.maps.Polyline({
-//             path: google.maps.geometry.encoding.decodePath(completeRoute),
-//             map: map
-//         });
-//         var bounds = new google.maps.LatLngBounds();
-//         for (var i = 0; i < polyline.getPath().getLength(); i++) {
-//             bounds.extend(polyline.getPath().getAt(i));
-//         }
-//         map.fitBounds(bounds);
-//     });
-// }
-
-function initMap() {
+async function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
-    center: {lat:41.38879, lng: 2.15899},
+    center: {lat:41.38879, lng: 2.15899}, //coords of Barcelona
   });
-  
-  $.getJSON("./resources/polyline.json", function(routes) {
-    console.log(google.maps.geometry);
+  routes = await $.getJSON("./resources/polyline.json", (routes)=> routes);
+  routes.forEach((route) => {
+      if (route.type === "route") {
+          addPolyline(route, map);
+      }
+      else if(route.type === "stop"){
+          var pos = {
+              lat: route.start.lat,
+              lng: route.start.lng
+          }
+          addMarker(pos,map);          
+      }
+  });
+}
 
-    routes.forEach((route) => {
-        if (route.type === "route") {
-            console.log(route.polyline);
-            var polyline = new google.maps.Polyline({
-                path: google.maps.geometry.encoding.decodePath(route.polyline),
-                map: map
-            });
-            var bounds = new google.maps.LatLngBounds();
-            for (var i = 0; i < polyline.getPath().getLength(); i++) {
-                bounds.extend(polyline.getPath().getAt(i));
-            }
-            map.fitBounds(bounds);
-        }
-        else if(route.type === "stop"){
-            var pos = {
-                lat: route.start.lat,
-                lng: route.start.lng
-            }
-            var marker = new google.maps.Marker({
-                map: map,
-                draggable: false,
-                position: pos,
-                icon: 'https://www.freeiconspng.com/uploads/stop-sign-png-14.png'
-            })
-        }
+function addMarker(coords, map){
+    var marker = new google.maps.Marker({
+        map: map,
+        draggable: false,
+        position: coords,
+        icon: 'https://maps.gstatic.com/mapfiles/ms2/micons/red-pushpin.png'
+    })
+}
+
+function addPolyline(route, map){
+    var polyline = new google.maps.Polyline({
+        path: google.maps.geometry.encoding.decodePath(route.polyline),
+        map: map
     });
-    // console.log(completeRoute);
 
-    
-});
-
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < polyline.getPath().getLength(); i++) {
+        bounds.extend(polyline.getPath().getAt(i));
+    }
+    map.fitBounds(bounds);
 }
 
 
-// var infoWindow = new google.maps.infoWindow({content: 'Vehicle Stop' });
 
-            // marker.addListener('click', ()=>{
-            //     infoWindow.open(map, marker);
-            // })
-
-
-//   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//   const markers = locations.map((location, i) => {
-//     return new google.maps.Marker({
-//       position: location,
-//       label: labels[i % labels.length],
-//     });
-//   });
-//   new MarkerClusterer(map, markers, {
-//     imagePath:
-//       "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-//   });
-
-
-const locations = [
-  { lat: -31.56391, lng: 147.154312 },
-  { lat: -33.718234, lng: 150.363181 },
-  { lat: -33.727111, lng: 150.371124 },
-  { lat: -33.848588, lng: 151.209834 },
-  { lat: -33.851702, lng: 151.216968 },
-  { lat: -34.671264, lng: 150.863657 },
-  { lat: -35.304724, lng: 148.662905 },
-  { lat: -36.817685, lng: 175.699196 },
-  { lat: -36.828611, lng: 175.790222 },
-  { lat: -37.75, lng: 145.116667 },
-  { lat: -37.759859, lng: 145.128708 },
-  { lat: -37.765015, lng: 145.133858 },
-  { lat: -37.770104, lng: 145.143299 },
-  { lat: -37.7737, lng: 145.145187 },
-  { lat: -37.774785, lng: 145.137978 },
-  { lat: -37.819616, lng: 144.968119 },
-  { lat: -38.330766, lng: 144.695692 },
-  { lat: -39.927193, lng: 175.053218 },
-  { lat: -41.330162, lng: 174.865694 },
-  { lat: -42.734358, lng: 147.439506 },
-  { lat: -42.734358, lng: 147.501315 },
-  { lat: -42.735258, lng: 147.438 },
-  { lat: -43.999792, lng: 170.463352 },
-];
